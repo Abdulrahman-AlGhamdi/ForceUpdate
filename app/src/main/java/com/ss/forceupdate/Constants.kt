e38797eab751db4726a319f84c02d825adc342de
Abdulrahman Al-Ghamdi <49812896+Abdulrahman-AlGhamdi@users.noreply.github.com>
2021-03-14 20:22:54 +0300

    fixing bugs

---
+++ app/src/main/java/com/ss/forceupdate/Constants.kt
@@ -0,0 +1,9 @@
+package com.ss.forceupdate
+
+object Constants {
+    const val EXTRA_TITLE = "title"
+    const val EXTRA_MESSAGE = "message"
+    const val EXTRA_APK_LINK = "link"
+    const val EXTRA_LOGO_IMAGE = "logo"
+    const val EXTRA_APPLICATION_NAME = "application name"
+}
--- app/src/main/java/com/ss/forceupdate/MainActivity.kt
+++ app/src/main/java/com/ss/forceupdate/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.ss.forceupdate
 
-import android.app.Activity
-import android.app.DownloadManager
-import android.content.IntentFilter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ss.forceupdate.databinding.ActivityMainBinding
@@ -10,27 +7,21 @@ import com.ss.forceupdate.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var forceUpdate: ForceUpdate
-    private lateinit var intentFilter: IntentFilter
-    val PACKAGE_INSTALLED_ACTION = "com.ss.forceupdate.SESSION_API_PACKAGE_INSTALLED"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        forceUpdate = ForceUpdate(this, this)
-        intentFilter = IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
-        forceUpdate.isApplicationUpdated("1.1")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        registerReceiver(forceUpdate.downloadBroadCastReceiver, intentFilter)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        unregisterReceiver(forceUpdate.downloadBroadCastReceiver)
+        ForceUpdateManager(
+            activity = this,
+            context = applicationContext,
+            newVersion = "1.1",
+            apkLink = "https://storage.evozi.com/apk/dl/16/09/04/com.soundcloud.android_65060.apk",
+            title = "New Update",
+            message = "New Update Available",
+            logo = R.drawable.google_play_logo,
+            applicationName = getString(R.string.app_name)
+            ).isApplicationUpdated()
     }
 }
