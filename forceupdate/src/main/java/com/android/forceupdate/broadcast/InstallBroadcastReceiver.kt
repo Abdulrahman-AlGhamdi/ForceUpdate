f4072d8226df8bcac12f4ee80316f0c46fcb7018
Abdulrahman-AlGhamdi <xxd7ooom.ssxx@gmail.com>
2021-04-10 11:04:41 +0300

    create a broadcast that can receive the status of the install and add the appropriate action for each status

---
+++ forceupdate/src/main/java/com/android/forceupdate/broadcast/InstallBroadcastReceiver.kt
@@ -0,0 +1,52 @@
+package com.android.forceupdate.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.EXTRA_INTENT
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.pm.PackageInstaller.*
+import android.util.Log
+import com.android.forceupdate.broadcast.InstallBroadcastReceiver.InstallStatus.*
+import com.android.forceupdate.repository.ForceUpdateRepositoryImpl.Companion.LOCAL_FILE
+import kotlinx.coroutines.flow.MutableStateFlow
+import java.io.File
+
+class InstallBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val localFile = intent.getSerializableExtra(LOCAL_FILE) as File?
+
+        when (intent.getIntExtra(EXTRA_STATUS, -1)) {
+            STATUS_PENDING_USER_ACTION -> {
+                val installIntent = intent.getParcelableExtra(EXTRA_INTENT) as Intent?
+                context.startActivity(installIntent?.addFlags(FLAG_ACTIVITY_NEW_TASK))
+            }
+            STATUS_SUCCESS -> {
+                localFile?.delete()
+                mutableInstallStatus.value = InstallSucceeded
+            }
+            STATUS_FAILURE_ABORTED -> {
+                localFile?.delete()
+                mutableInstallStatus.value = InstallCanceled
+            }
+            else -> {
+                localFile?.delete()
+                val message = intent.getStringExtra(EXTRA_STATUS_MESSAGE)
+                mutableInstallStatus.value = InstallError(message.toString())
+            }
+        }
+    }
+
+    sealed class InstallStatus {
+        object InstallInitialized : InstallStatus()
+        object InstallSucceeded : InstallStatus()
+        object InstallCanceled : InstallStatus()
+        data class InstallError(val message: String) : InstallStatus()
+    }
+
+    companion object {
+        val mutableInstallStatus = MutableStateFlow<InstallStatus>(InstallInitialized)
+        val installStatus get() = mutableInstallStatus
+    }
+}
--- forceupdate/src/main/java/com/android/forceupdate/repository/ForceUpdateRepositoryImpl.kt
+++ forceupdate/src/main/java/com/android/forceupdate/repository/ForceUpdateRepositoryImpl.kt
@@ -2,26 +2,29 @@ package com.android.forceupdate.repository
 
 import android.app.Application
 import android.app.DownloadManager
-import android.app.PendingIntent
+import android.app.DownloadManager.*
+import android.app.PendingIntent.*
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageInstaller
+import android.content.pm.PackageInstaller.*
 import android.net.Uri
 import androidx.core.content.FileProvider
+import androidx.documentfile.provider.DocumentFile
+import com.android.forceupdate.broadcast.InstallBroadcastReceiver
+import com.android.forceupdate.broadcast.InstallBroadcastReceiver.*
+import com.android.forceupdate.repository.ForceUpdateRepositoryImpl.DownloadStatus.*
 import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.channels.sendBlocking
-import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.io.File
 
 class ForceUpdateRepositoryImpl(private val application: Application) : ForceUpdateRepository {
 
     override suspend fun downloadApk(apkLink: String) = flow {
-        val request = DownloadManager.Request(Uri.parse(apkLink)).apply {
+        val request = Request(Uri.parse(apkLink)).apply {
             this.setAllowedOverRoaming(true)
             this.setAllowedOverMetered(true)
-            this.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+            this.setNotificationVisibility(Request.VISIBILITY_VISIBLE)
             this.setDestinationInExternalFilesDir(application, null, "update.apk")
         }
 
@@ -30,7 +33,7 @@ class ForceUpdateRepositoryImpl(private val application: Application) : ForceUpd
         val id = sharedPreferences.getLong(DOWNLOAD_ID_KEY, -1L)
         downloadManager.remove(id)
         val downloadId = downloadManager.enqueue(request)
-        val query = DownloadManager.Query().setFilterById(downloadId)
+        val query = Query().setFilterById(downloadId)
 
         sharedPreferences.edit().apply {
             this.putLong(DOWNLOAD_ID_KEY, downloadId)
@@ -43,27 +46,27 @@ class ForceUpdateRepositoryImpl(private val application: Application) : ForceUpd
                 val cursor = downloadManager.query(query)
                 if (cursor != null && cursor.count >= 0 && cursor.moveToFirst()) {
 
-                    val bytes = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
-                    val totalSize = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
-                    val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                    val bytes = cursor.getInt(cursor.getColumnIndex(COLUMN_BYTES_DOWNLOADED_SO_FAR))
+                    val totalSize = cursor.getInt(cursor.getColumnIndex(COLUMN_TOTAL_SIZE_BYTES))
+                    val status = cursor.getInt(cursor.getColumnIndex(COLUMN_STATUS))
                     val percentage = ((bytes.toDouble() / totalSize) * 100).toInt()
 
                     withContext(Dispatchers.Main) {
-                        this@flow.emit(DownloadStatus.DownloadProgress(percentage))
+                        this@flow.emit(DownloadProgress(percentage))
                     }
 
-                    if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                    if (status == STATUS_SUCCESSFUL) {
                         withContext(Dispatchers.Main) {
                             isDownloading = false
-                            val uri = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
-                            this@flow.emit(DownloadStatus.DownloadCompleted(File(Uri.parse(uri).path!!)))
+                            val uri = cursor.getString(cursor.getColumnIndex(COLUMN_LOCAL_URI))
+                            this@flow.emit(DownloadCompleted(File(Uri.parse(uri).path!!)))
                             this.cancel()
                         }
                     }
                 } else {
                     withContext(Dispatchers.Main) {
                         isDownloading = false
-                        this@flow.emit(DownloadStatus.DownloadCanceled)
+                        this@flow.emit(DownloadCanceled)
                         this.cancel()
                     }
                 }
@@ -77,53 +80,37 @@ class ForceUpdateRepositoryImpl(private val application: Application) : ForceUpd
         object DownloadCanceled : DownloadStatus()
     }
 
-    override fun installApk(localFile: File) = callbackFlow<InstallStatus> {
+    override fun installApk(localFile: File): Flow<InstallStatus> {
         val contentUri = FileProvider.getUriForFile(application, application.packageName, localFile)
         val packageInstaller = application.packageManager.packageInstaller
-        val sessionParams = PackageInstaller.SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL)
-        val sessionId = packageInstaller.createSession(sessionParams)
-        val session: PackageInstaller.Session = packageInstaller.openSession(sessionId)
+        val contentResolver = application.contentResolver
 
-        val intent = Intent(Intent.ACTION_VIEW).apply {
-            this.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_ACTIVITY_NEW_TASK
-            this.setDataAndType(contentUri, INSTALL_TYPE)
+        contentResolver.openInputStream(contentUri)?.use { apkStream ->
+            val length = DocumentFile.fromSingleUri(application, contentUri)?.length() ?: -1
+            val sessionParams = SessionParams(SessionParams.MODE_FULL_INSTALL)
+            val sessionId = packageInstaller.createSession(sessionParams)
+            val session = packageInstaller.openSession(sessionId)
+
+            session.openWrite(localFile.name, 0, length).use { sessionStream ->
+                apkStream.copyTo(sessionStream)
+                session.fsync(sessionStream)
+            }
+
+            val intent = Intent(application, InstallBroadcastReceiver::class.java).apply {
+                this.putExtra(LOCAL_FILE, localFile)
+            }
+
+            val pendingIntent = getBroadcast(application, 2, intent, FLAG_UPDATE_CURRENT)
+            session.commit(pendingIntent.intentSender)
+            session.close()
         }
 
-        application.startActivity(intent)
-
-        val pendingIntent = PendingIntent.getActivity(application, 0, intent, 0)
-        val statusReceiver = pendingIntent.intentSender
-        session.commit(statusReceiver)
-        session.abandon()
-        session.close()
-
-        packageInstaller.registerSessionCallback(object : PackageInstaller.SessionCallback() {
-            override fun onCreated(sessionId: Int) {
-            }
-            override fun onBadgingChanged(sessionId: Int) {
-            }
-            override fun onActiveChanged(id: Int, active: Boolean) {
-            }
-            override fun onProgressChanged(sessionId: Int, progress: Float) {
-                val installProgress = ((progress / 0.90000004) * 100).toInt()
-                sendBlocking(InstallStatus.InstallProgress(installProgress))
-            }
-            override fun onFinished(id: Int, success: Boolean) {
-                sendBlocking(InstallStatus.InstallFinished(success))
-            }
-        })
-
-        awaitClose()
-    }
-
-    sealed class InstallStatus {
-        data class InstallProgress(val progress: Int) : InstallStatus()
-        data class InstallFinished(val isSuccess: Boolean) : InstallStatus()
+        return InstallBroadcastReceiver.installStatus
     }
 
     companion object {
+        const val LOCAL_FILE = "Local File"
         private const val DOWNLOAD_ID_KEY = "Download"
         private const val DOWNLOAD_ID_NAME = "Download ID"
-        private const val INSTALL_TYPE = "application/vnd.android.package-archive"
     }
 }
